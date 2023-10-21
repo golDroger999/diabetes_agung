d28e2cc6a2d17462ec66b9884e6fc6d09cf8e0fb
golDroger999 <goldr1711@gmail.com>
2023-10-21 19:23:56 +0800

    push

--- app.py
+++ app.py
@@ -31,27 +31,13 @@ def main():
         with col5 : 
             umur = st.number_input('masukan usia',1)
             # f_stress = st.number_input('faktor stress', 1.0)
-    perkeni = func.perkeni(berat_badan=bb, tinggi_badan=tb, umur=umur, gender=gender, faktor_aktivitas=f_aktiv)   
-    # with st.expander('NOTE PROMPT'):
-    #     st.write('---------------------')    
-    #     # st.subheader('diagnosa penyakit')
-    #     keluhan = st.text_input(' DIAGNOSA PENYAKIT PROMPT EDITOR / NOTE')
-    #     st.write('---------------------')
-    #     biokimia = st.text_area('BIOKIMIA PROMPT EDITOR')
-    #     st.write('---------------------')
-    #     fisik_klinis = st.text_area('FISIK KLINIS PROMPT EDITOR ')
-    #     st.write('---------------------')
-    #     st.subheader('RIWAYAT ASUPAN GIZI')
-    #     e, p, l, k = st.columns(4)
-    #     with e: riwayat_energi = st.number_input('RIWAYAT ENERGI')
-    #     with p: riwayat_protein = st.number_input('RIWAYAT PROTEIN')
-    #     with l: riwayat_lemak = st.number_input('RIWAYAT LEMAK')
-    #     with k: riwayat_karbo = st.number_input('RIWAYAT KARBO')
-    #     st.write('---------------------')
-        
-        
-        
     
+    with st.expander('ANTROPOMETRI'):
+        col1a, col2a = st.columns(2)
+        with col1a: gdp  = st.number_input('gula darah puasa',1)
+        with col1a: gds  = st.number_input('gula darah sewaktu',1)
+        
+    perkeni = func.perkeni(berat_badan=bb, tinggi_badan=tb, umur=umur, gender=gender, faktor_aktivitas=f_aktiv)   
     if st.button('ANALISA'):
             
         st.subheader('Hasil Perhitungan Gizi')
@@ -79,14 +65,7 @@ def main():
                 # st.success(f'CAIRAN : {cairan}')
                 
                 
-            # with col9:
-            #     st.subheader('RIWAYAT')
-            #     st.success(f'ENERGI : {riwayat_energi}')
-            #     st.success(f'PROTEIN : {riwayat_protein}')
-            #     st.success(f'LEMAK : {riwayat_lemak}')
-            #     st.success(f'KARBOHIDRAT : {riwayat_karbo}')
-              
-            # st.success(modelbot(kata=keluhan))     
+    
         with st.expander('KEBUTUHAN ZAT GIZI SEKALI MAKAN '):
                 col3, col4, col5 = st.columns(3)
                 with col3:
@@ -108,116 +87,6 @@ def main():
                     st.success(f'ENERGI : {perkeni["energi_malam"]}')
                     st.success(f'PROTEIN : {perkeni["protein_malam"]}')
                     st.success(f'LEMAK : {perkeni["lemak_malam"]}')
-                    st.success(f'KARBOHIDRAT : {perkeni["karbo_malam"]}') 
-    
-    #     try:                    
-    #         penyakit = modelbot(keluhan)                
-    #         with st.expander('LIHAT JAWABAN'):
-    #             sound_file = BytesIO()
-    #             tts = gTTS(f"{penyakit['response']}", lang='id')
-    #             tts.write_to_fp(sound_file)
-    #             st.success(f"({penyakit['tag']}):")
-    #             st.success(f"({penyakit['akurasi']}):")
-    #             st.audio(sound_file)
-    #             st.markdown(f"<p style='text-align: justify;'>{penyakit['response']}</p>", unsafe_allow_html=True)
-    #     except:
-    #         st.write('none')
-    #         pass                
-        
-
-                   
-    #     hasil = klasifikasi_biokimia(biokimia)         
-    #     if isinstance(hasil, dict):
-    #         st.subheader('Hasil Klasifikasi Biokimia')
-    #         for nama_biokimia, data in hasil.items():  
-    #             with st.expander(f'{nama_biokimia}'):
-    #                 st.info(nama_biokimia)
-    #                 col6, col7, col8 =  st.columns(3)
-    #                 with col6: st.success(f"Nilai: {data['nilai']}")
-    #                 with col7: st.success(f"Kategori: {data['kategori']}")
-    #                 with col8: st.success(f"rujukan: {data['rujukan']}")
-    #                 if data['kategori'] != 'normal':
-    #                     st.subheader('penjelasan')
-    #                     sound_file = BytesIO()
-    #                     tts = gTTS(f"{data['note']}", lang='id')
-    #                     tts.write_to_fp(sound_file)
-    #                     st.audio(sound_file)
-    #                     st.write(f"<p style='text-align:justify'>{data['anjuran']}</p>", unsafe_allow_html=True)
-                         
-    #     fisik = klasifikasi_fisik_klinis(fisik_klinis)         
-    #     if isinstance(fisik, dict):
-    #         st.subheader('Hasil Klasifikasi Fisik Klinis')
-    #         for nama_fisik_klinis, data in fisik.items():  
-    #             with st.expander(f'{nama_fisik_klinis}'):
-    #                 st.info(nama_fisik_klinis)
-    #                 col6, col7, col8 =  st.columns(3)
-    #                 with col6: st.success(f"Nilai: {data['nilai']}")
-    #                 with col7: st.success(f"Kategori: {data['kategori']}")
-    #                 with col8: st.success(f"rujukan: {data['rujukan']}")
-    #                 if data['kategori'] != 'normal':
-    #                     st.subheader('penjelasan')
-    #                     sound_file = BytesIO()
-    #                     tts = gTTS(f"{data['note']}", lang='id')
-    #                     tts.write_to_fp(sound_file)
-    #                     st.audio(sound_file)
-    #                     st.write(f"<p style='text-align:justify'>{data['anjuran']}</p>", unsafe_allow_html=True)
-                         
-
-    
-    # if st.button('LAPORAN'):
-    #     antro = {
-    #         'bbi' : int(bbi),
-    #         'imt' : int(imt),
-    #         'bb' : int(bb) 
-    #     }
-        
-    #     hasil = klasifikasi_biokimia(biokimia)
-    #     table=[]
-    #     try:
-    #         for nama_biokimia, data in hasil.items():
-    #             table.append({
-    #             'nama': nama_biokimia,
-    #             'nilai': data['nilai'],
-    #             'hasil': data['kategori'],
-    #             'rujukan': data['rujukan'],
-    #             'satuan' : data['satuan'],
-    #             'rujukan' : data['rujukan'],
-    #             'note' : data['note'],
-    #             'anjuran' : data['anjuran']            
-    #             })
-    #     except:
-    #         pass
-
-    #     data_fk = klasifikasi_fisik_klinis(fisik_klinis)
-    #     fk_table=[]
-    #     try:
-    #         for nama_fisik_klinis, data in data_fk.items():
-    #             fk_table.append({
-    #             'nama': nama_fisik_klinis,
-    #             'nilai': data['nilai'],
-    #             'hasil': data['kategori'],
-    #             'rujukan': data['rujukan'],
-    #             'satuan' : data['satuan'],
-    #             'rujukan' : data['rujukan'],
-    #             'note' : data['note'],
-    #             'anjuran' : data['anjuran']            
-    #             })
-    #     except:
-    #         pass
-        
-    #     generate_doc(nama, umur, bb, tb, f_aktiv, 
-    #              gender, imt, bbi, bee, tee, protein, lemak, karbo, 
-    #              energi_pagi, protein_pagi, lemak_pagi, karbo_pagi,
-    #              energi_malam, protein_malam, lemak_malam, karbo_malam,
-    #              keluhan,pembahasan="modelbot(kata=keluhan)", mikro=mikro, biokimia=table,
-    #              antro=antro, fisik_klinis=fk_table)
-        
-    #     with open('generate.docx', 'rb') as f:
-    #         st.download_button(label='Download Generated Document',
-    #                             data=f,
-    #                             file_name=f'{nama}.docx',
-    #                             mime='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
-
-
+   
 if __name__ == "__main__":
     main()
